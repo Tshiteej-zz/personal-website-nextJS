fa99043be9f1d49984db0acf55232f435caad974
Tshiteej Bhardwaj <tshiteejnitskm@gmail.com>
2021-03-23 22:09:05 +0530

    first version

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,40 @@ import {
   ColorModeProvider,
   useColorMode
 } from "@chakra-ui/react";
+import { Global, css } from "@emotion/react";
+
 import customTheme from "../styles/theme";
+
+const GlobalStyle = ({ children }) => {
+  const { colorMode } = useColorMode();
+  return (
+    <>
+      <Global
+        styles={css`
+          ::selection {
+            background-color: #90cdf4;
+            color: #fefefe;
+          }
+          ::-moz-selection {
+            background: #ffb7b7;
+            color: #fefefe;
+          }
+          html {
+            min-width: 356px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+            background: ${colorMode === "light" ? "white" : "#171717"};
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -14,7 +47,9 @@ function MyApp({ Component, pageProps }) {
           useSystemColorMode: true
         }}
       >
-        <Component {...pageProps} />
+        <GlobalStyle>
+          <Component {...pageProps} />
+        </GlobalStyle>
       </ColorModeProvider>
     </ChakraProvider>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,48 @@
 import Head from "next/head";
-import { Heading } from "@chakra-ui/react";
+import { useColorMode, Heading, Text, Flex, Stack } from "@chakra-ui/react";
+
+import Container from "../components/Container";
 
 export default function Index() {
-  return <Heading as='h1'>INDEX</Heading>;
+  const { colorMode } = useColorMode();
+  const colorSecondary = {
+    light: "gray.700",
+    dark: "gray.400"
+  };
+  return (
+    <Container>
+      <Head>
+        <title>Tshiteej Bhardwaj</title>
+      </Head>
+      <Stack
+        as='main'
+        spacing={8}
+        justifyContent='center'
+        alignItems='flex-start'
+        m='0 auto 4rem auto'
+        maxWidth='700px'
+        px={2}
+      >
+        <Flex
+          flexDirection='column'
+          justifyContent='flex-start'
+          alignItems='flex-start'
+          maxWidth='700px'
+        >
+          <Heading mb={2}>Hi, I'm Tshiteej</Heading>
+          <Text color={colorSecondary[colorMode]}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </Text>
+        </Flex>
+      </Stack>
+    </Container>
+  );
 }
